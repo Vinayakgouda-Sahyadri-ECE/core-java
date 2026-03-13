0feb99ce2aecbbc1ecedf2223b2f8db692096419
Vinayakgouda-Sahyadri-ECE <shadowvinayak@gmail.com>
2026-03-13 10:53:32 +0530

    uploaded

---
+++ Jacket.java
@@ -0,0 +1,17 @@
+class Jacket {
+    String brand;
+    String size;
+    String material;
+    String color;
+    boolean hasHood;
+
+    Jacket() {}
+
+    Jacket(String brand, String size, String material, String color, boolean hasHood) {
+        this.brand = brand;
+        this.size = size;
+        this.material = material;
+        this.color = color;
+        this.hasHood = hasHood;
+    }
+}
